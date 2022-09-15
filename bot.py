2d0ca30631f830a5a2be9094037be384ac116b83
Logan Apple <loganapple@hotmail.com>
2022-09-14 19:33:10 -0700

    Update release version

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-VERSION_NUMBER = "09102022" # Just for tracking releases
+VERSION_NUMBER = "09142022" # Just for tracking releases
 
 class BenjaminBowtieBot(commands.Bot):
     def __init__(self):
