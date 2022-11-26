1a94b8eb49b99d5a85df023b93a11ca1133cce2d
Logan Apple <loganapple@hotmail.com>
2022-11-25 20:07:27 -0800

    Singular string support

--- features/house/garden.py
+++ features/house/garden.py
@@ -218,7 +218,7 @@ class GardenPlot():
         for plant_key_1, plant_key_2 in MUTATION_PROBS.keys():
             if self.seed_data.result == plant_key_1 or self.seed_data.result == plant_key_2:
                 num_mutations += 1
-        mutate_string = f"{num_mutations} possible mutations\n" if num_mutations > 0 else ""
+        mutate_string = f"{num_mutations} possible mutation{'s' if num_mutations != 1 else ''}\n" if num_mutations > 0 else ""
 
         display_string = f"\n\n──────────\n{self.seed.get_full_name()}\n\n{self.growth_ticks} {growth_tick_str} old\n{mature_string}\n{mutate_string}──────────"
 
