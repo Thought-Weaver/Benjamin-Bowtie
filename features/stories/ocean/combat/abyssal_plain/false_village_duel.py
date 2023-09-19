e925b80d2ebae81bd67e322a10d2d775a6045c01
Logan Apple <loganapple540@gmail.com>
2023-09-18 22:29:49 -0700

    Fixed another false village bug

--- features/stories/ocean/combat/abyssal_plain/false_village_duel.py
+++ features/stories/ocean/combat/abyssal_plain/false_village_duel.py
@@ -51,15 +51,15 @@ class OceanFinalWordsView(discord.ui.View):
             player = self._get_player(user.id)
             ps = player.get_stats().dungeon_runs
 
-            ps.forest_rooms_explored += self._dungeon_run.rooms_explored
-            ps.forest_combat_encounters += self._dungeon_run.combat_encounters
-            ps.forest_treasure_rooms_encountered += self._dungeon_run.treasure_rooms_encountered
-            ps.forest_shopkeeps_encountered += self._dungeon_run.shopkeeps_encountered
-            ps.forest_events_encountered += self._dungeon_run.events_encountered
-            ps.forest_rests_taken += self._dungeon_run.rests_taken
-            ps.forest_bosses_defeated += self._dungeon_run.bosses_defeated
-            ps.forest_adventures_won += 1
-            ps.forest_adventures_played += 1
+            ps.ocean_rooms_explored += self._dungeon_run.rooms_explored
+            ps.ocean_combat_encounters += self._dungeon_run.combat_encounters
+            ps.ocean_treasure_rooms_encountered += self._dungeon_run.treasure_rooms_encountered
+            ps.ocean_shopkeeps_encountered += self._dungeon_run.shopkeeps_encountered
+            ps.ocean_events_encountered += self._dungeon_run.events_encountered
+            ps.ocean_rests_taken += self._dungeon_run.rests_taken
+            ps.ocean_bosses_defeated += self._dungeon_run.bosses_defeated
+            ps.ocean_adventures_won += 1
+            ps.ocean_adventures_played += 1
 
     def get_initial_embed(self):
         post_run_info_str: str = self._generate_run_info()
